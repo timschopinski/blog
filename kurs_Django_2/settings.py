d9dde692eb58a9efe0deabcbdd532dc28223fab7
timschopinski <timschopinski@gmail.com>
2022-06-16 15:15:55 +0200

    env set

--- kurs_Django_2/settings.py
+++ kurs_Django_2/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from os import getenv
+import environ
 
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,14 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5!4+hz$#nr$1r-s!+qmj1(+9&9hm_g$ifptu!4c#diyf60x-&b'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = getenv("IS_DEVELOPMENT", True)
+DEBUG = env("IS_DEVELOPMENT")
 
 ALLOWED_HOSTS = [
-    getenv("APP_HOST", "127.0.0.1")
+    env("APP_HOST")
 ]
 
 
@@ -77,23 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kurs_Django_2.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'djangoblog',
-#         'PASSWORD': 'komunikator',
-#         'HOST': 'django-blog.cdrxfsajowsa.us-east-1.rds.amazonaws.com',
-#         'PORT': '5432'
-#     }
-# }
-
-
-#default database connection
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
